069ae9569c9d69773572eafb2b68bdcae73be4be
Diego Garzon <digarodev@gmail.com>
2020-10-19 15:43:06 -0500

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    1. Basic annotations - concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+        Function that concat two arguments.
+
+        Arguments:
+            str1 (str): A parameter string
+            str2 (str): A parameter string
+
+        Return:
+            str: Concatenated string
+    '''
+    return str1 + str2
