ab61cfbda7bdfa9bf36b9f316c326670e6f6b042
Diego Garzon <digarodev@gmail.com>
2020-10-19 16:18:43 -0500

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+    3. Basic annotations - to string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+        Function that return the string representation of the argment
+
+        Arguments:
+            n (float): A float number
+
+        Return:
+            str: the string representation of the argument
+    '''
+    return str(n)
