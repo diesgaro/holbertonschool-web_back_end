f8803d1413c9b2ee8615e7dc22c65497c0230df7
Diego Garzon <digarodev@gmail.com>
2020-10-19 15:32:49 -0500

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    0. Basic annotations - add
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Function that sum two arguments.
+
+        Arguments:
+            a (float): A float number
+            b (float): A float number
+
+        Return:
+            float: The sum of the arguments
+    '''
+    return a + b
