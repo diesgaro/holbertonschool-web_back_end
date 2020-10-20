dc6172891f2b4142b5ef8160429c3805505ff204
Diego Garzon <digarodev@gmail.com>
2020-10-20 17:49:34 -0500

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+1. Let's execute multiple coroutines at the same time with async
+'''
+
+
+import asyncio
+import bisect
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(max_delay: int, n: int) -> float:
+    '''
+    Async routine that spawn wait_random n times with the specified max_delay
+    '''
+    lst: List = []
+    i: int = 0
+    while i < n:
+        bisect.insort(lst, await wait_random(max_delay))
+        # lst.append(await wait_random(max_delay))
+        i += 1
+
+    return lst
