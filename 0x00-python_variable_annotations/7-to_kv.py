03f46300457edbde240f0f32480f9d8faa8fdf5e
Diego Garzon <digarodev@gmail.com>
2020-10-19 22:04:44 -0500

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+    6. Complex types - mixed list
+'''
+
+from typing import Union, Tuple
+import math
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+        Function that takes str and int or float arguments and return a
+        tuple with the str and te square of int/float
+
+        Arguments:
+            k (str): A string argument
+            v (int/float): a Integer or Float argument
+            
+
+        Return:
+            Tuple: a tuple with the result
+    '''
+
+    return (k, math.pow(v, 2))
