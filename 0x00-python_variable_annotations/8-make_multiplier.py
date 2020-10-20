86cbbadd7c43233ffa702fb146b366e093248517
Diego Garzon <digarodev@gmail.com>
2020-10-19 22:43:45 -0500

    fix white spaces

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,7 @@
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''
         Function that make a function to multiplies a float argument
@@ -25,7 +26,6 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
             Return:
                 Float: the result of multiplies
         '''
-
         return n * multiplier
-    
-    return f    
+
+    return f
