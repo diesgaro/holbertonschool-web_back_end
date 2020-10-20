f939863d437d4f2af15840142e4a6b57cf68de07
Diego Garzon <digarodev@gmail.com>
2020-10-20 18:07:16 -0500

    fix, move the order of function parameters

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Async routine that spawn wait_random n times with the specified max_delay
     '''
