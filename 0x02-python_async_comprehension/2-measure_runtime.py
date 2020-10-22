50b11028ee8f3db4b4f3194a3ec6ec9d8f768d11
Diego Garzon <digarodev@gmail.com>
2020-10-21 23:24:06 -0500

    fix, add typing Coroutine

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,7 +5,7 @@
 
 import asyncio
 import time
-from typing import List, Coroutine
+from typing import List, Coroutine, Any
 
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
@@ -16,7 +16,11 @@ async def measure_runtime() -> float:
     and return the total runtime
     '''
 
-    tasks: List[Coroutine] = [async_comprehension() for n in range(4)]
+    tasks: List[
+        Coroutine[Any, None, None]
+    ] = [
+        async_comprehension() for n in range(4)
+    ]
 
     start: float = time.time()
 
