e80846cb170d5a258aad62c365709b49d3bf7710
Diego Garzon <digarodev@gmail.com>
2020-10-21 22:08:23 -0500

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+0. Async Generator
+'''
+
+import asyncio
+import random
+
+
+async def async_generator():
+    '''
+    Coroutine that loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10.
+    '''
+    remaining: int = 11
+
+    while True:
+        remaining -= 1
+
+        if not remaining:
+            break
+
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,13 @@
+# 0x02. Python - Async Comprehension
+
+## Resources
+
+* PEP 530 – Asynchronous Comprehensions
+* What’s New in Python: Asynchronous Comprehensions / Generators
+* Type-hints for generators
+
+## Learning Objectives
+
+* How to write an asynchronous generator
+* How to use async comprehensions
+* How to type-annotate generators
