d0e6410febf91e6b753534c751606f740fae2c83
Diego Garzon <digarodev@gmail.com>
2020-10-26 18:54:22 -0500

    task 1

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+1. FIFO caching
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Class FIFOCache that inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+        method initializer/constructor
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Method that add key:value to the dictionary self.cache_data
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                if (len(self.cache_data) == self.MAX_ITEMS):
+                    print("{} {}".format(
+                        'DISCARD:',
+                        next(iter(self.cache_data))
+                    ))
+                    del self.cache_data[next(iter(self.cache_data))]
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Method that return the value of dict self.cahce_data linked to key
+        """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        return None
